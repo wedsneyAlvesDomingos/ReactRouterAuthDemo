74cfa1ef31d6d3345c63f35d92c16d9f2e053241
wedsneyAlvesDomingos <105890092+wedsneyAlvesDomingos@users.noreply.github.com>
2024-04-25 14:30:16 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: '/login',
-  },
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     open: '/login',
+//   },
+// })
